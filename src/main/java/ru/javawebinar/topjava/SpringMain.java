8db0ef9e4f07738749fa385564997264106a1bb9
Sergii Malov <malov_s@ukr.net>
2016-01-26 14:47:24 +0200

    Add_spring_context_and_main

---
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -0,0 +1,18 @@
+package ru.javawebinar.topjava;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.repository.UserRepository;
+
+import java.util.Arrays;
+
+
+public class SpringMain {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        System.out.println(Arrays.toString(appCtx.getBeanDefinitionNames()));
+        UserRepository userRepository = (UserRepository) appCtx.getBean("mockUserRepository");
+        userRepository = appCtx.getBean(UserRepository.class);
+        appCtx.close();
+    }
+}
