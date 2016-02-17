36a635190d92c9700b7686348e598baa0516d544
Sergii Malov <malov_s@ukr.net>
2016-02-17 15:28:32 +0200

    HW6_fix_tests

---
+++ src/test/java/ru/javawebinar/topjava/service/AbstractJpaUserServiceTest.java
@@ -0,0 +1,18 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.javawebinar.topjava.repository.JpaUtil;
+
+
+abstract public class AbstractJpaUserServiceTest extends AbstractUserServiceTest {
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    private JpaUtil jpaUtil;
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        jpaUtil.clear2ndLevelHibernateCache();
+    }
+}
