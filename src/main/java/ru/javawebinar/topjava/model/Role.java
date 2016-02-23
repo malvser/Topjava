089fc3ff5550b643ac48f3418e69eb0391ca9ffa
Sergii Malov <malov_s@ukr.net>
2016-02-23 15:25:08 +0200

    auth_via_user_service

--- src/main/java/ru/javawebinar/topjava/model/Role.java
+++ src/main/java/ru/javawebinar/topjava/model/Role.java
@@ -1,7 +1,13 @@
 package ru.javawebinar.topjava.model;
 
+import org.springframework.security.core.GrantedAuthority;
 
-public enum Role {
+public enum Role implements GrantedAuthority {
     ROLE_USER,
-    ROLE_ADMIN
+    ROLE_ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
